bb5341e03419212c2bb157053a19815270eb6ff0
xkyLleex <leekwanyao710@gmail.com>
2020-05-23 16:15:20 +0800

    add animation

--- js/index.js
+++ js/index.js
@@ -1,18 +1,41 @@
 //git pull --rebase
 //git push origin HEAD:refs/heads/master
-//var index_timer = window.setInterval(indexIMG,5000);
+
+window.onload = function(){ 
+    document.getElementById("left").onmouseover = function(){mouse_over(true)};
+    document.getElementById("right").onmouseover = function(){mouse_over(true)};
+    document.getElementById("left").onmouseout = function(){mouse_over(false)};
+    document.getElementById("right").onmouseout = function(){mouse_over(false)};
+}
+
 var x = 0;
+var status = 0;
+var index_timer = window.setInterval(imgchange,10000);
+
+function mouse_over(check){
+    if(check)status = 1;
+    else status = 0;
+}
+
 function imgchange(text){
     var imglist = document.getElementsByTagName('li');
-    for(var i=0;i<imglist.length;i++){
-        imglist[i].className = 'imgnone';
-    }
-    if(text == "left"){
+    if(status == 0){
+        if(x == 0)imglist[imglist.length-1].className = 'imgnone';
+        else imglist[x-1].className = 'imgnone'; 
+        imglist[x].className = 'imghide';
+        x++;
+        if(x > imglist.length-1)x = 0;
+        imglist[x].className = 'imgshow';
+    }else if(text == "left"){
+        if(x == imglist.length-1)imglist[0].className = 'imgnone';
+        else imglist[x+1].className = 'imgnone'; 
         imglist[x].className = 'imghide';
         x--;
         if(x < 0)x = imglist.length-1;
         imglist[x].className = 'imgshow';
-    }else{
+    }else if(text == "right"){
+        if(x == 0)imglist[imglist.length-1].className = 'imgnone';
+        else imglist[x-1].className = 'imgnone'; 
         imglist[x].className = 'imghide';
         x++;
         if(x > imglist.length-1)x = 0;
---
+++ js/page.js
@@ -0,0 +1,6 @@
+function nav_click(){
+    document.getElementById("navlist").className = "menu_navlist"
+}
+window.onresize = function(){
+    
+}
